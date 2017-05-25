f5da98c48493a44c6fdb0903d304f8bb15a8406e
Babalola Tobi <easycom2011@gmail.com>
2017-05-25 13:25:44 +0100

    Let see if this works

--- config.js
+++ config.js
@@ -4,4 +4,8 @@ module.exports = {
     API_AI_CLIENT_ACCESS_TOKEN: '831414a36a584033af0fec82c0561e55',
     FB_APP_SECRET: 'e70cee9b0123ea9a16a3bd9d95bda2ff',
     SERVER_URL: "https://murmuring-basin-16009.herokuapp.com/",
+    SENGRID_API_KEY: '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',
+    EMAIL_FROM: 'easycom2011@gmail.com',
+    EMAIL_TO: 'easycom2011@gmail.com',
+    WEATHER_API_KEY: '3b57f575975f3304eb4216403ede6680',
 };
